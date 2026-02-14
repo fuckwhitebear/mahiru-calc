f44228f139f4dcd75433d408ed926d0016935dba
Mahiru Shiina <mahiru@angel.next.door>
2026-02-14 14:37:55 +0000

    Initial commit by Mahiru ❤️

---
+++ script.js
@@ -0,0 +1,77 @@
+let currentOperand = '0';
+let previousOperand = '';
+let operation = undefined;
+
+const currentOperandTextElement = document.getElementById('current-operand');
+const previousOperandTextElement = document.getElementById('previous-operand');
+
+function updateDisplay() {
+    currentOperandTextElement.innerText = currentOperand;
+    if (operation != null) {
+        previousOperandTextElement.innerText = `${previousOperand} ${operation}`;
+    } else {
+        previousOperandTextElement.innerText = '';
+    }
+}
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    if (currentOperand === '0' && number !== '.') {
+        currentOperand = number.toString();
+    } else {
+        currentOperand = currentOperand.toString() + number.toString();
+    }
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+    updateDisplay();
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '×':
+            computation = prev * current;
+            break;
+        case '÷':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '0';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteNumber() {
+    currentOperand = currentOperand.toString().slice(0, -1);
+    if (currentOperand === '') currentOperand = '0';
+    updateDisplay();
+}
